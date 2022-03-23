adec850cdc4cbe2db424157e297ff5a8bec8f0b3
Ganesh Suthar <ganesh@appentus.me>
2022-03-23 15:06:41 +0530

    rinkby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xaf358fa1cF53891b82CC2dAf93E35B924878BC26"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
